925455dabeb5b4efa3ba28a81074e36be37f9c68
RikkeKKH <rikkekkh205@gmail.com>
2024-05-06 12:52:48 +0200

    Commit

--- Assets/Scenes/Scripts/LavFunktion.cs
+++ Assets/Scenes/Scripts/LavFunktion.cs
@@ -21,9 +21,15 @@ public class LavFunktion : MonoBehaviour
     //int rundeNr=0;
 
     //public TextMeshProUGUI[] TextListe;
-    string[] svarmuligheder;
+    List <string> svarmuligheder=new List<string>{"","","",""};
+    List <string> svarmuligheder1;
+    List <string> svarmuligheder2;
+    List <string> svarmuligheder3;
+    List <string> svarmuligheder4;
     public List<TextMeshProUGUI> TextListe;
     List<int> PladsOptaget = new List<int>();
+
+    int test =0;
     
 
     
@@ -48,29 +54,25 @@ public class LavFunktion : MonoBehaviour
 
     
        
-        var rigtigt1 =(n*a)+"x<sup>"+nsup;
+        var r1 =(n*a)+"x<sup>"+nsup;
     
 
 
-        var forkert1 =(n*a)+"x<sup>"+(nsup);
-        string forkert1s=forkert1.ToString();
-
-        var forkert2 =((n-1)*a)+"x<sup>"+(nsup);
-        string forkert2s=forkert2.ToString();
+        var f12 =((n-1)*a)+"x<sup>"+(nsup);
 
 
         int w=Random.Range(2,10);
         if (a==n&&w==n-1){
-            w=w+1;
+            w=w+2;
             
         }
-        var forkert3 =(a*w+n)+"x<sup>"+(nsup);
-        string forkert3s=forkert3.ToString();
+        var f13 =(a*w+n)+"x<sup>"+(nsup);
+
+        var f11 =(a*w-n)+"x<sup>"+(nsup);
 
 
-        string rigtig1s=rigtigt1.ToString();
 
-        svarmuligheder = new string[]{rigtig1s,forkert1s,forkert2s,forkert3s};
+        svarmuligheder1 = new List<string>{r1.ToString(),f11.ToString(),f12.ToString(),f13.ToString()};
     
     
         
@@ -78,29 +80,67 @@ public class LavFunktion : MonoBehaviour
 
         int b=Random.Range(2,31);
 
-        int rigtigt2 =b;
+        int r2 =b;
+
+        int f21 =Random.Range(2,31);
+        if (f21==b){
+            f21=f21+1;
+        }
+        int f22=f21+b;
+
+        string f23="x";
+
+        svarmuligheder2 = new List<string>{r2.ToString(),f21.ToString(),f22.ToString(),f23.ToString()};
+
 
 
         int s=Random.Range(0,4);
 
-        string rigtigt3="";
+        string r3="";
+        string f31="";
+        string f32="";
+        string f33="";
 
         if (s==0){
-            rigtigt3="cos(x)";
+            r3="cos(x)";
+            f31="-sin(x)";
+            f32="-cos(x)";
+            f33="sin(x)";
         }
         else if (s==1){
-            rigtigt3="-sin(x)";
+            r3="-sin(x)";
+            f31="-cos(x)";
+            f32="sin(x)";
+            f33="cos(x)";
         }
         else if (s==2){
-            rigtigt3="e<sup>x</sup>";
+            r3="e<sup>x</sup>";
+            f31="ln(x)";
+            f32="x<sup>e</sup>";
+            f33="tan(x)";
         }
         else if (s==3){
-            rigtigt3="1/x";
+            r3="1/x";
+            f31="ln(x)";
+            f32="e<sup>x</sup>";
+            f33="x(ln)";
         }
+
+        svarmuligheder3 = new List<string>{r3.ToString(),f31.ToString(),f32.ToString(),f33.ToString()};
 
         int k=Random.Range(2,31);
 
-        int rigtigt4=0;
+        int r4=0;
+
+        int f41=k;
+
+        int f42=Random.Range(2,31);
+        if (f42==k){
+            f42=f42*2;
+        }
+        string f43="x";
+
+        svarmuligheder4 = new List<string>{r4.ToString(),f41.ToString(),f42.ToString(),f43.ToString()};
         
         
 
@@ -118,7 +158,31 @@ public class LavFunktion : MonoBehaviour
     void Update()
     {   
 
-        if (Input.GetKeyDown("J")){
+        if (Input.GetKeyDown(KeyCode.J)){
+            test=test+1;
+            svarmuligheder.Clear();
+
+            if(test==1){
+                for (int i=0;i<4;i++){
+                    svarmuligheder.Add(svarmuligheder1[i]);
+                }
+            }
+            if(test==2){
+                for (int i=0;i<4;i++){
+                    svarmuligheder.Add(svarmuligheder2[i]);
+                }
+            }
+            if(test==3){
+                for (int i=0;i<4;i++){
+                    svarmuligheder.Add(svarmuligheder3[i]);
+                }
+            }
+            if(test==4){
+                for (int i=0;i<4;i++){
+                    svarmuligheder.Add(svarmuligheder4[i]);
+                }
+            }
+
 
         }
         
